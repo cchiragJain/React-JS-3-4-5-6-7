e6e71d36e414363101205cae1beae9cee95676af
Chirag <cjain8060@gmail.com>
2020-09-07 14:49:25 +0530

    section-4 till conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       { name: 'b', age: 27 },
       { name: 'c', age: 25 },
     ],
-    someValue: 'someValue',
+    showPersons: false,
   };
 
   switchNameHandler = newName => {
@@ -32,6 +32,13 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    // simply changes the value of showPersons whenever the togglePersonHandler is fired
+    this.setState({
+      showPersons: !this.state.showPersons,
+    });
+  };
+
   render() {
     const buttonStyle = {
       backgroundColor: 'white',
@@ -40,32 +47,63 @@ class App extends Component {
       cursor: 'pointer',
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={() => this.switchNameHandler('from arrow syntax')}
+            changed={this.nameChangeHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button
-          style={buttonStyle}
-          onClick={this.switchNameHandler.bind(this, 'from bind')}
-        >
-          Switch Name
+        <button style={buttonStyle} onClick={this.togglePersonsHandler}>
+          Switch Persons
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={() => this.switchNameHandler('from arrow syntax')}
-          changed={this.nameChangeHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        {persons}
+        {/* 
+					USING TERNARY OPERATORS TO CONDITIONALLY RENDER CONTENT
+					remeber that only simple statements work we cannot write a if else statement here to dynamically render content therefore using ternary expression
+
+					{this.state.showPersons ? (
+						<div>
+							<Person
+								name={this.state.persons[0].name}
+								age={this.state.persons[0].age}
+							/>
+							<Person
+								name={this.state.persons[1].name}
+								age={this.state.persons[1].age}
+								click={() => this.switchNameHandler('from arrow syntax')}
+								changed={this.nameChangeHandler}
+							>
+								My Hobbies: Racing
+							</Person>
+							<Person
+								name={this.state.persons[2].name}
+								age={this.state.persons[2].age}
+							/>
+						</div>
+					) : null} */}
       </div>
     );
   }
