2df2547fa9d5d842c5374b82aa1c9c0f39027076
Chirag <cjain8060@gmail.com>
2020-09-07 17:35:30 +0530

    section-5 initial

--- src/App.js
+++ src/App.js
@@ -25,15 +25,12 @@ class App extends Component {
   };
 
   togglePersonsHandler = () => {
-    // simply changes the value of showPersons whenever the togglePersonHandler is fired
     this.setState({
       showPersons: !this.state.showPersons,
     });
   };
 
   deletePersonHandler = personIndex => {
-    // copying the array using the spread operator to not mutate the state directly
-    // will not show a error without copying though but it is a better approach
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({
@@ -54,7 +51,6 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {/* OUTPUTTING LISTS */}
           {this.state.persons.map((person, index) => {
             return (
               <Person
@@ -62,28 +58,10 @@ class App extends Component {
                 age={person.age}
                 click={() => this.deletePersonHandler(index)}
                 key={person.id}
-                // the anonymous function is what actually gets called so therefore it will have the event property can use index as well but we have id's now so use that
                 changed={event => this.nameChangeHandler(event, person.id)}
               />
             );
           })}
-          {/* HARD CODED VALUES 
-					<Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler('from arrow syntax')}
-            changed={this.nameChangeHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          /> */}
         </div>
       );
     }
@@ -96,35 +74,9 @@ class App extends Component {
           Switch Persons
         </button>
         {persons}
-        {/* 
-					USING TERNARY OPERATORS TO CONDITIONALLY RENDER CONTENT
-					remeber that only simple statements work we cannot write a if else statement here to dynamically render content therefore using ternary expression
-
-					{this.state.showPersons ? (
-						<div>
-							<Person
-								name={this.state.persons[0].name}
-								age={this.state.persons[0].age}
-							/>
-							<Person
-								name={this.state.persons[1].name}
-								age={this.state.persons[1].age}
-								click={() => this.switchNameHandler('from arrow syntax')}
-								changed={this.nameChangeHandler}
-							>
-								My Hobbies: Racing
-							</Person>
-							<Person
-								name={this.state.persons[2].name}
-								age={this.state.persons[2].age}
-							/>
-						</div>
-					) : null} */}
       </div>
     );
   }
 }
 
 export default App;
-// Functional Component -> Stateless, dumb, presentational
-// Class Based Component -> Stateful, smart, container
