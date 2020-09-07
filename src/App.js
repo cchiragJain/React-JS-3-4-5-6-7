e47498790c36c71fc43daf11b262b200790fa9da
Chirag <cjain8060@gmail.com>
2020-09-07 12:44:25 +0530

    initial commit section-4

--- src/App.js
+++ src/App.js
@@ -13,15 +13,7 @@ class App extends Component {
   };
 
   switchNameHandler = newName => {
-    // console.log('clicked');
-    // DONT DO THIS: this.state.persons[0].name = 'd';
-
     this.setState({
-      // persons: [
-      //   { name: 'd', age: 28 },
-      //   { name: 'b', age: 27 },
-      //   { name: 'c', age: 28 },
-      // ],
       persons: [
         { name: newName, age: 28 },
         { name: 'b', age: 27 },
@@ -41,7 +33,6 @@ class App extends Component {
   };
 
   render() {
-    // we can use inline styles rather than declaring new css files but it has some restrictions of not being able to use pseudo-selectors like hover etc. in inline styles.
     const buttonStyle = {
       backgroundColor: 'white',
       border: '3px solid green',
@@ -53,9 +44,6 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        {/* one way to pass data is to use the bind keyword to solve the this keyword issue 
-				other way is to use the arrow syntax 
-				however the arrow syntax can be slower in a big app due to constant re rendering by react */}
         <button
           style={buttonStyle}
           onClick={this.switchNameHandler.bind(this, 'from bind')}
@@ -80,7 +68,6 @@ class App extends Component {
         />
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
